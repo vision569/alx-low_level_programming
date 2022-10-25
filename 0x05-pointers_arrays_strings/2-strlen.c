38cfb0f6f3c43d378aa74ff14e6e6975d9785607
vision569 <asowgwuclinton@gmail.com>
2022-10-25 08:56:17 -0700

    task 3

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,8 +1,13 @@
+/*
+ * File: 2-strlen.c
+ */
+
 #include "main.h"
+
 /**
  * _strlen - Returns the length of a string.
+
  * @str: The string to get the length of.
- *
  * Return: The length of @str.
  */
 
